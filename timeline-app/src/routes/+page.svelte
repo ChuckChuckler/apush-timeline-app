0616c9b7644c0db270596e64be4e6f52adacfb13
Avishi Sharma <robloxpusheen8@gmail.com>
2026-04-27 21:25:49 -0400

    check by century

--- timeline-app/src/routes/+page.svelte
+++ timeline-app/src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
     let includedEvents:string[]=[]
 
+    let display1600s:string=$state("display:none");
+    let display1700s:string=$state("display:none");
+    let display1800s:string=$state("display:none");
+    let display1900s:string=$state("display:none");
+
     let events:Record<string,string[]>={
         "Pre-1600":[
             "Pre-European contact",
@@ -268,6 +273,47 @@
             includedEvents.splice(includedEvents.indexOf(period),1);
         }
     }
+
+    let checkAll1600s:boolean=$state(false);
+    let checkAll1700s:boolean=$state(false);
+    let checkAll1800s:boolean=$state(false);
+    let checkAll1900s:boolean=$state(false);
+
+    function checkAll(century:string, checked:boolean){
+        let century1600:string[] = ["First half of 1600s", "Second half of 1600s"];
+        let century1700:string[] = ["1700-60", "1770-79", "1780-89", "1790-99"];
+        let century1800:string[] = ["1800-19", "1820-29", "1830-39", "1840-49", "1850-59", "1860-69", "1870-79", "1880-89", "1890-99"];
+        let century1900:string[] = ["1900-09", "1910-19", "1920-29", "1930-39", "1940-49", "1950-59", "1960-69", "1970-79", "1980-89", "1990-99"];
+
+        let chosenCentury:string[] = [];
+
+        if(century=="1600s"){
+            chosenCentury=century1600;
+            checked?checkAll1600s=true:checkAll1600s=false;
+        }else if(century=="1700s"){
+            chosenCentury=century1700;
+            checked?checkAll1700s=true:checkAll1700s=false;
+        }else if(century=="1800s"){
+            chosenCentury=century1800;
+            checked?checkAll1800s=true:checkAll1800s=false;
+        }else if(century=="1900s"){
+            chosenCentury=century1900;
+            checked?checkAll1900s=true:checkAll1900s=false;
+        }
+
+        if(checked){
+            for(let i in chosenCentury){
+                includedEvents.push(chosenCentury[i])
+            }
+        }else{
+            for(let i in chosenCentury){
+                includedEvents.splice(includedEvents.indexOf(chosenCentury[i]),1);
+            }
+        }
+
+        console.log(includedEvents);
+    }
+    
 </script>
 <h1 class="text-center">Really fun practice app (so much fun)</h1>
 <div class="w-[50%] m-auto">
@@ -287,92 +333,113 @@
             <input type="checkbox" name="Pre-1600" id="Pre-1600" value="Pre-1600" onchange={function(){include("Pre-1600", this.checked==undefined?false:this.checked)}}>
             <label for="Pre-1600">Pre-1600</label>
             <br>
-            <div>
-                <input type="checkbox" name="First half of 1600s" id="First half of 1600s" value="First half of 1600s" onchange={function(){include("First half of 1600s", this.checked==undefined?false:this.checked)}}>
+            <br>
+            <input type="checkbox" name="1600s" value="1600s" id="1600s" onchange={function(){this.checked==true?checkAll("1600s", true):checkAll("1600s",false)}}>
+            <label for="1600s">1600s</label>
+            <button onclick={function(){display1600s=="display:none"?display1600s="display:block":display1600s="display:none"}}>Expand/close</button>
+            <div class="ml-50px" style={display1600s}>
+                <input checked={checkAll1600s} type="checkbox" name="First half of 1600s" id="First half of 1600s" value="First half of 1600s" onchange={function(){include("First half of 1600s", this.checked==undefined?false:this.checked)}}>
                 <label for="First half of 1600s">First half of 1600s</label>
                 <br>
-                <input type="checkbox" name="Second half of 1600s" id="Second half of 1600s" value="Second half of 1600s" onchange={function(){include("Second half of 1600s", this.checked==undefined?false:this.checked)}}>
+                <input checked={checkAll1600s} type="checkbox" name="Second half of 1600s" id="Second half of 1600s" value="Second half of 1600s" onchange={function(){include("Second half of 1600s", this.checked==undefined?false:this.checked)}}>
                 <label for="Second half of 1600s">Second half of 1600s</label>
             </div>
             <br>
-            <div>
-                <input type="checkbox" name="1700-60" id="1700-60" value="1700-60" onchange={function(){include("1700-60", this.checked==undefined?false:this.checked)}}>
+            <br>
+
+            <input type="checkbox" name="1700s" id="1700s" value="1700s" onchange={function(){this.checked==true?checkAll("1700s", true):checkAll("1700s",false)}}>
+            <label for="1700s">1700s</label>
+            <button onclick={function(){display1700s=="display:none"?display1700s="display:block":display1700s="display:none"}}>Expand/close</button>
+            <div class="ml-50px" style={display1700s}>
+                <input type="checkbox" name="1700-60" id="1700-60" value="1700-60" checked={checkAll1700s} onchange={function(){include("1700-60", this.checked==undefined?false:this.checked)}}>
                 <label for="1700-60">1700-60</label>
                 <br>
-                <input type="checkbox" name="1760-69" id="1760-69" value="1760-69" onchange={function(){include("1760-69", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1760-69" id="1760-69" value="1760-69" checked={checkAll1700s} onchange={function(){include("1760-69", this.checked==undefined?false:this.checked)}}>
                 <label for="1760-69">1760-69</label>
                 <br>
-                <input type="checkbox" name="1770-79" id="1770-79" value="1770-79" onchange={function(){include("1770-79", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1770-79" id="1770-79" value="1770-79" checked={checkAll1700s} onchange={function(){include("1770-79", this.checked==undefined?false:this.checked)}}>
                 <label for="1770-79">1770-79</label>
                 <br>
-                <input type="checkbox" name="1780-89" id="1780-89" value="1780-89" onchange={function(){include("1780-89", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1780-89" id="1780-89" value="1780-89" checked={checkAll1700s} onchange={function(){include("1780-89", this.checked==undefined?false:this.checked)}}>
                 <label for="1780-89">1780-89</label>
                 <br>
-                <input type="checkbox" name="1790-99" id="1790-99" value="1790-99" onchange={function(){include("1790-99", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1790-99" id="1790-99" value="1790-99" checked={checkAll1700s} onchange={function(){include("1790-99", this.checked==undefined?false:this.checked)}}>
                 <label for="1790-99">1790-99</label>
             </div>
             <br>
-            <div>
-                <input type="checkbox" name="1800-19" id="1800-19" value="1800-19" onchange={function(){include("1800-19", this.checked==undefined?false:this.checked)}}>
+            <br>
+
+            <input type="checkbox" name="1800s" value="1800s" id="1800s" onchange={function(){this.checked==true?checkAll("1800s", true):checkAll("1800s",false)}}>
+            <label for="1800s">1800s</label>
+            <button onclick={function(){display1800s=="display:none"?display1800s="display:block":display1800s="display:none"}}>Expand/close</button>
+            <div style={display1800s}>
+                <input type="checkbox" name="1800-19" id="1800-19" value="1800-19" checked={checkAll1800s} onchange={function(){include("1800-19", this.checked==undefined?false:this.checked)}}>
                 <label for="1800-19">1800-19</label>
                 <br>
-                <input type="checkbox" name="1820-29" id="1820-29" value="1820-29" onchange={function(){include("1820-29", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1820-29" id="1820-29" value="1820-29" checked={checkAll1800s} onchange={function(){include("1820-29", this.checked==undefined?false:this.checked)}}>
                 <label for="1820-29">1820-29</label>
                 <br>
-                <input type="checkbox" name="1830-39" id="1830-39" value="1830-39" onchange={function(){include("1830-39", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1830-39" id="1830-39" value="1830-39" checked={checkAll1800s} onchange={function(){include("1830-39", this.checked==undefined?false:this.checked)}}>
                 <label for="1830-39">1830-39</label>
                 <br>
-                <input type="checkbox" name="1840-49" id="1840-49" value="1840-49" onchange={function(){include("1840-49", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1840-49" id="1840-49" value="1840-49" checked={checkAll1800s} onchange={function(){include("1840-49", this.checked==undefined?false:this.checked)}}>
                 <label for="1840-49">1840-49</label>
                 <br>
-                <input type="checkbox" name="1850-59" id="1850-59" value="1850-59" onchange={function(){include("1850-59", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1850-59" id="1850-59" value="1850-59" checked={checkAll1800s} onchange={function(){include("1850-59", this.checked==undefined?false:this.checked)}}>
                 <label for="1850-59">1850-59</label>
                 <br>
-                <input type="checkbox" name="1860-69" id="1860-69" value="1860-69" onchange={function(){include("1860-69", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1860-69" id="1860-69" value="1860-69" checked={checkAll1800s} onchange={function(){include("1860-69", this.checked==undefined?false:this.checked)}}>
                 <label for="1860-69">1860-69</label>
                 <br>
-                <input type="checkbox" name="1870-79" id="1870-79" value="1870-79" onchange={function(){include("1870-79", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1870-79" id="1870-79" value="1870-79" checked={checkAll1800s} onchange={function(){include("1870-79", this.checked==undefined?false:this.checked)}}>
                 <label for="1870-79">1870-79</label>
                 <br>
-                <input type="checkbox" name="1880-89" id="1880-89" value="1880-89" onchange={function(){include("1880-89", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1880-89" id="1880-89" value="1880-89" checked={checkAll1800s} onchange={function(){include("1880-89", this.checked==undefined?false:this.checked)}}>
                 <label for="1880-89">1880-89</label>
                 <br>
-                <input type="checkbox" name="1890-99" id="1890-99" value="1890-99" onchange={function(){include("1890-99", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1890-99" id="1890-99" value="1890-99" checked={checkAll1800s} onchange={function(){include("1890-99", this.checked==undefined?false:this.checked)}}>
                 <label for="1890-99">1890-99</label>
             </div>
             <br>
-            <div>
-                <input type="checkbox" name="1900-09" id="1900-09" value="1900-09" onchange={function(){include("1900-09", this.checked==undefined?false:this.checked)}}>
+            <br>
+
+            <input type="checkbox" name="1900s" value="1900s" id="1900s" onchange={function(){this.checked==true?checkAll("1900s", true):checkAll("1900s",false)}}>
+            <label for="1900s">1900s</label>
+            <button onclick={function(){display1900s=="display:none"?display1900s="display:block":display1900s="display:none"}}>Expand/close</button>
+            <div style={display1900s}>
+                <input type="checkbox" name="1900-09" id="1900-09" value="1900-09" checked={checkAll1900s} onchange={function(){include("1900-09", this.checked==undefined?false:this.checked)}}>
                 <label for="1900-09">1900-09</label>
                 <br>
-                <input type="checkbox" name="1910-19" id="1910-19" value="1910-19" onchange={function(){include("1910-19", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1910-19" id="1910-19" value="1910-19" checked={checkAll1900s} onchange={function(){include("1910-19", this.checked==undefined?false:this.checked)}}>
                 <label for="1910-19">1910-19</label>
                 <br>
-                <input type="checkbox" name="1920-29" id="1920-29" value="1920-29" onchange={function(){include("1920-29", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1920-29" id="1920-29" value="1920-29" checked={checkAll1900s} onchange={function(){include("1920-29", this.checked==undefined?false:this.checked)}}>
                 <label for="1920-29">1920-29</label>
                 <br>
-                <input type="checkbox" name="1930-39" id="1930-39" value="1930-39" onchange={function(){include("1930-39", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1930-39" id="1930-39" value="1930-39" checked={checkAll1900s} onchange={function(){include("1930-39", this.checked==undefined?false:this.checked)}}>
                 <label for="1930-09">1930-39</label>
                 <br>
-                <input type="checkbox" name="1940-49" id="1940-49" value="1940-49" onchange={function(){include("1940-49", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1940-49" id="1940-49" value="1940-49" checked={checkAll1900s} onchange={function(){include("1940-49", this.checked==undefined?false:this.checked)}}>
                 <label for="1940-49">1940-49</label>
                 <br>
-                <input type="checkbox" name="1950-59" id="1950-59" value="1950-59" onchange={function(){include("1950-59", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1950-59" id="1950-59" value="1950-59" checked={checkAll1900s} onchange={function(){include("1950-59", this.checked==undefined?false:this.checked)}}>
                 <label for="1950-59">1950-59</label>
                 <br>
-                <input type="checkbox" name="1960-69" id="1960-69" value="1960-69" onchange={function(){include("1960-69", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1960-69" id="1960-69" value="1960-69" checked={checkAll1900s} onchange={function(){include("1960-69", this.checked==undefined?false:this.checked)}}>
                 <label for="1960-69">1960-69</label>
                 <br>
-                <input type="checkbox" name="1970-79" id="1970-79" value="1970-79" onchange={function(){include("1970-79", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1970-79" id="1970-79" value="1970-79" checked={checkAll1900s} onchange={function(){include("1970-79", this.checked==undefined?false:this.checked)}}>
                 <label for="1970-79">1970-79</label>
                 <br>
-                <input type="checkbox" name="1980-89" id="1980-89" value="1980-89" onchange={function(){include("1980-89", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1980-89" id="1980-89" value="1980-89" checked={checkAll1900s} onchange={function(){include("1980-89", this.checked==undefined?false:this.checked)}}>
                 <label for="1980-89">1980-89</label>
                 <br>
-                <input type="checkbox" name="1990-99" id="1990-99" value="1990-99" onchange={function(){include("1990-99", this.checked==undefined?false:this.checked)}}>
+                <input type="checkbox" name="1990-99" id="1990-99" value="1990-99" checked={checkAll1900s} onchange={function(){include("1990-99", this.checked==undefined?false:this.checked)}}>
                 <label for="1990-99">1900-09</label>
             </div>
             <br>
+            <br>
+
             <input type="checkbox" name="2000-" id="2000-" value="2000-" onchange={function(){include("2000-", this.checked==undefined?false:this.checked)}}>
             <label for="2000-">2000-</label>
             <br>
